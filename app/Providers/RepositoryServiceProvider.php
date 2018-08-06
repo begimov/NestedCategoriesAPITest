6c84149e51fa95931fd60982c91ddab1f95f0961
begimov <begimov@gmail.com>
2018-08-06 14:56:18 +0300

    ProductRepository::class binded to its eloquent implementation in RepositoryServiceProvider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,18 +2,27 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\{
+    ProductRepository
+};
+
+use App\Repositories\Eloquent\{
+    EloquentProductRepository
+};
 use League\Container\ServiceProvider\AbstractServiceProvider;
 
 class RepositoryServiceProvider extends AbstractServiceProvider
 {
     protected $provides = [
-        //
+        ProductRepository::class
     ];
 
     public function register()
     {
         $container = $this->getContainer();
         
-        //
+        $container->share(ProductRepository::class, function() {
+            return new EloquentProductRepository();
+        });
     }
 }
