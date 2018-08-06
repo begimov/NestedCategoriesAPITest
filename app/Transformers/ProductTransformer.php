b4ec40ed28760ce358c5d3c171e8ae7e733c7183
begimov <begimov@gmail.com>
2018-08-06 14:28:52 +0300

    basic ProductTransformer has bee created

---
+++ app/Transformers/ProductTransformer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\Product;
+
+class ProductTransformer extends \League\Fractal\TransformerAbstract
+{
+    public function transform(Product $product)
+    {
+        return [
+            'id' => $product->id,
+        ];
+    }
+}
